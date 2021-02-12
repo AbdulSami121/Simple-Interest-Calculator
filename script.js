105c8e371673287b89fcc33d4533bfc3b91b5023
Devan Bhagat <devan.bhagat@gmail.com>
2021-02-12 12:16:14 -0500

    starting compute function

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function showrate(x)
 {
     document.getElementById("enteredRate").innerText = x;
 }
+function compute()
+{
+    var principal = document.getElementById("principal");
+    var rate = document.getElementById("rate");
+    var years = document.getElementById("years");
+    var interest = principal * years * rate / 100;
+    
+}
