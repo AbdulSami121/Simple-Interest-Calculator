982c4b33c5f6893cc9ba4df60615c5a462c42dcd
Devan Bhagat <devan.bhagat@gmail.com>
2021-02-12 16:30:20 -0500

    Complete compute function needs validation

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
 function showrate(x)
 {
     document.getElementById("enteredRate").innerText = x;
 }
 function compute()
 {
-    var principal = document.getElementById("principal");
-    var rate = document.getElementById("rate");
-    var years = document.getElementById("years");
+    var principal = parseFloat(document.getElementById("principal").value,10);
+    var rate = parseFloat(document.getElementById("rate").value,10);
+    var years = parseFloat(document.getElementById("years").value,10);
     var interest = principal * years * rate / 100;
-    
+    var currentYear = new Date().getFullYear();
+    var finalYear = currentYear + years;
+    document.getElementById("result").innerHTML = "<br />If you deposit "+principal+",<br /> at an interest rate of "+rate+"%.<br />You will receive an amount of "+interest+"<br />in the year "+finalYear+"<br />";
 }
